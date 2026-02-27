7a0f422a7faa395b22fce58645f16bce2b91de84
= <=>
2026-02-27 11:50:05 +0530

    Initial commit

--- src/main/java/com/javatechie/crud/SpringbootCrudK8sExampleApplication.java
+++ src/main/java/com/javatechie/crud/SpringbootCrudK8sExampleApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootCrudK8sExampleApplication {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootCrudK8sExampleApplication.class, args);
-	}
-
+        SpringApplication.run(SpringbootCrudK8sExampleApplication.class, args);
+    }
 }
